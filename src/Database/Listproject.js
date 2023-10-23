7659663ac194d218a03ea48113c2f514260e77b6
Brayan Alexander Salazar Reyes <bsalazarreyes88@gmail.com>
2023-10-23 16:45:30 -0500

    update

--- src/Database/Listproject.js
+++ src/Database/Listproject.js
@@ -1,5 +1,5 @@
 export const mostrarProject = async () =>{
-    const res = await fetch("../database/bd.json")
+    const res = await fetch("https://my-json-server.typicode.com/BrayanAlexanderSalazarReyes12/DB-Portafolio/Project")
     const result = await res.json()
     return result
 }
